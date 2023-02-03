8deb3adc4759900bb2e996cb9a131134f1865bb9
DANIEL MACHARIA <73613987+Fill-Daniel@users.noreply.github.com>
2023-02-03 19:07:47 +0300

    Create new script.js

---
+++ new script.js
@@ -0,0 +1,17 @@
+
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <script src='https://8x8.vc/vpaas-magic-cookie-9704d43e21b64752977cba99086c273d/external_api.js' async></script>
+        <style>html, body, #jaas-container { height: 100%; }</style>
+        <script type="text/javascript">
+          window.onload = () => {
+            const api = new JitsiMeetExternalAPI("8x8.vc", {
+              roomName: "vpaas-magic-cookie-9704d43e21b64752977cba99086c273d/SampleAppInclinedRecessionsWakeUnder",
+              parentNode: document.querySelector('#jaas-container')
+            });
+          }
+        </script>
+      </head>
+      <body><div id="jaas-container" /></body>
+    </html>
